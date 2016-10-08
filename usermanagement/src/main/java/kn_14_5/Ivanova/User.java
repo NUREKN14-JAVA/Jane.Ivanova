6a7eb53551da02e7f81bb146e3c961f35c121094
JaneIvanova <yevheniia.ivanova@nure.ua>
2016-10-09 00:33:24 +0300

    Initial commit for usermanagement

---
+++ usermanagement/src/main/java/kn_14_5/Ivanova/User.java
@@ -0,0 +1,48 @@
+package kn_14_5.Ivanova;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class User {
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getFullName() {
+		return getLastName() + ", " + getFirstName();
+	}
+	public int getAge() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		int currentYear = calendar.get(Calendar.YEAR);
+		calendar.setTime(getDateOfBirth());
+		int year = calendar.get(Calendar.YEAR);
+		return currentYear - year;
+	}
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Date dateOfBirth;
+	
+}
---
+++ usermanagement/src/test/java/kn_14_5/Ivanova/UserTest.java
@@ -0,0 +1,32 @@
+package kn_14_5.Ivanova;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+	private User user;
+	private Date dateOfBirth;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		user=new User();
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1984, Calendar.MAY, 26);
+		dateOfBirth=calendar.getTime();
+	}
+	public void testGetFullName() {
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		assertEquals("Doe, John",user.getFullName());
+	}
+	
+	public void testGetAge(){
+		user.setDateOfBirth(dateOfBirth);
+		assertEquals(2016 - 1984, user.getAge());
+
+	}
+}
+
